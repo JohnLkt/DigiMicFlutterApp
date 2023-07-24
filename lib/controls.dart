6d5515a1eb62f92d195d167fecfe76b8e6856700
Psicko <alvinnothing15@gmail.com>
2023-07-24 15:41:38 +0700

    Update

--- lib/controls.dart
+++ lib/controls.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field, prefer_final_fields
-
-import 'dart:async';
-import 'package:circle_button/circle_button.dart';
 import 'package:digimicapp/model.dart';
 import 'package:digimicapp/staticclass.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +6,7 @@ import 'package:provider/provider.dart';
 import 'component/slide.dart';
 
 class newControlPanel extends StatefulWidget {
-  const newControlPanel({
-    super.key,
-  });
+  const newControlPanel({super.key});
 
   @override
   State<newControlPanel> createState() => _newControlPanelState();
@@ -21,24 +15,33 @@ class newControlPanel extends StatefulWidget {
 class _newControlPanelState extends State<newControlPanel> {
   int indexTop = 0;
   double valueBottom = 20;
-  TextEditingController _brightnessController = TextEditingController();
-  TextEditingController _xposController = TextEditingController();
-  TextEditingController _yposController = TextEditingController();
-  TextEditingController _zposController = TextEditingController();
+  TextEditingController _brightnessController =
+      TextEditingController(text: '0');
+  TextEditingController _xposController = TextEditingController(text: '0');
+  TextEditingController _yposController = TextEditingController(text: '0');
+  TextEditingController _zposController = TextEditingController(text: '0');
 
-  void _incrementValue(TextEditingController controller) {
+  void _incrementValue(TextEditingController controller, int maxValue) {
     final int currentValue = int.tryParse(controller.text) ?? 0;
     final int newValue = currentValue + Variable.slidervalue;
-    controller.text = newValue.toString();
+    if (newValue <= maxValue) {
+      controller.text = newValue.toString();
+    } else {
+      controller.text = maxValue.toString();
+    }
     controller.selection = TextSelection.fromPosition(
       TextPosition(offset: controller.text.length),
     );
   }
 
-  void _incrementValueMin(TextEditingController controller) {
+  void _incrementValueMin(TextEditingController controller, int minValue) {
     final int currentValue = int.tryParse(controller.text) ?? 0;
     final int newValue = currentValue - Variable.slidervalue;
-    controller.text = newValue.toString();
+    if (newValue >= minValue) {
+      controller.text = newValue.toString();
+    } else {
+      controller.text = minValue.toString();
+    }
     controller.selection = TextSelection.fromPosition(
       TextPosition(offset: controller.text.length),
     );
@@ -65,19 +68,21 @@ class _newControlPanelState extends State<newControlPanel> {
           Row(
             children: [
               IconButton(
-                  onPressed: () {
-                    _incrementValueMin(_brightnessController);
-                    espDataState
-                        .updateBrightness(_brightnessController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_left)),
+                onPressed: () {
+                  _incrementValueMin(_brightnessController, 0);
+                  final brightness = int.tryParse(_brightnessController.text);
+                  if (brightness != null) {
+                    espDataState.updateBrightness(brightness);
+                  }
+                },
+                icon: Icon(Icons.arrow_left),
+              ),
               Expanded(
                 child: TextField(
                   controller: _brightnessController,
                   onChanged: (newBrightness) {
                     if (newBrightness.trim().isEmpty) {
-                      espDataState.updateBrightness(
-                          0); // Set a default value when input is empty
+                      espDataState.updateBrightness(0);
                     } else {
                       final brightness = int.tryParse(newBrightness);
                       if (brightness != null) {
@@ -85,20 +90,29 @@ class _newControlPanelState extends State<newControlPanel> {
                       }
                     }
                   },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      labelText: 'Enter Brightness'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    labelText: 'Enter Brightness',
+                  ),
                 ),
               ),
               IconButton(
-                  onPressed: () {
-                    _incrementValue(_brightnessController);
-                    espDataState
-                        .updateBrightness(_brightnessController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_right))
+                onPressed: () {
+                  _incrementValue(_brightnessController,
+                      255); // Replace 100 with your max value
+                  final brightness = int.tryParse(_brightnessController.text);
+                  if (brightness != null) {
+                    espDataState.updateBrightness(brightness);
+                  }
+                },
+                icon: Icon(Icons.arrow_right),
+              ),
             ],
           ),
           const SizedBox(
@@ -107,18 +121,19 @@ class _newControlPanelState extends State<newControlPanel> {
           Row(
             children: [
               IconButton(
-                  onPressed: () {
-                    _incrementValueMin(_xposController);
-                    espDataState.updateXPos(_brightnessController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_left)),
+                onPressed: () {
+                  _incrementValueMin(_xposController, 0);
+                  espDataState
+                      .updateXPos(int.tryParse(_xposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_left),
+              ),
               Expanded(
                 child: TextField(
                   controller: _xposController,
                   onChanged: (newXPos) {
                     if (newXPos.trim().isEmpty) {
-                      espDataState.updateXPos(
-                          0); // Set a default value when input is empty
+                      espDataState.updateXPos(0);
                     } else {
                       final xpos = int.tryParse(newXPos);
                       if (xpos != null) {
@@ -126,19 +141,27 @@ class _newControlPanelState extends State<newControlPanel> {
                       }
                     }
                   },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      labelText: 'Enter X Position'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    labelText: 'Enter X Position',
+                  ),
                 ),
               ),
               IconButton(
-                  onPressed: () {
-                    _incrementValue(_xposController);
-                    espDataState.updateXPos(_xposController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_right))
+                onPressed: () {
+                  _incrementValue(
+                      _xposController, 6000); // Replace 100 with your max value
+                  espDataState
+                      .updateXPos(int.tryParse(_xposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_right),
+              ),
             ],
           ),
           const SizedBox(
@@ -147,18 +170,19 @@ class _newControlPanelState extends State<newControlPanel> {
           Row(
             children: [
               IconButton(
-                  onPressed: () {
-                    _incrementValueMin(_yposController);
-                    espDataState.updateYPos(_yposController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_left)),
+                onPressed: () {
+                  _incrementValueMin(_yposController, 0);
+                  espDataState
+                      .updateYPos(int.tryParse(_yposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_left),
+              ),
               Expanded(
                 child: TextField(
                   controller: _yposController,
                   onChanged: (newYPos) {
                     if (newYPos.trim().isEmpty) {
-                      espDataState.updateYPos(
-                          0); // Set a default value when input is empty
+                      espDataState.updateYPos(0);
                     } else {
                       final ypos = int.tryParse(newYPos);
                       if (ypos != null) {
@@ -166,19 +190,27 @@ class _newControlPanelState extends State<newControlPanel> {
                       }
                     }
                   },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      labelText: 'Enter Y Position'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    labelText: 'Enter Y Position',
+                  ),
                 ),
               ),
               IconButton(
-                  onPressed: () {
-                    _incrementValue(_yposController);
-                    espDataState.updateYPos(_yposController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_right))
+                onPressed: () {
+                  _incrementValue(
+                      _yposController, 6000); // Replace 100 with your max value
+                  espDataState
+                      .updateYPos(int.tryParse(_yposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_right),
+              ),
             ],
           ),
           const SizedBox(
@@ -187,18 +219,19 @@ class _newControlPanelState extends State<newControlPanel> {
           Row(
             children: [
               IconButton(
-                  onPressed: () {
-                    _incrementValueMin(_zposController);
-                    espDataState.updateZPos(_zposController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_left)),
+                onPressed: () {
+                  _incrementValueMin(_zposController, 0);
+                  espDataState
+                      .updateZPos(int.tryParse(_zposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_left),
+              ),
               Expanded(
                 child: TextField(
                   controller: _zposController,
                   onChanged: (newZPos) {
                     if (newZPos.trim().isEmpty) {
-                      espDataState.updateZPos(
-                          0); // Set a default value when input is empty
+                      espDataState.updateZPos(0);
                     } else {
                       final zpos = int.tryParse(newZPos);
                       if (zpos != null) {
@@ -206,19 +239,27 @@ class _newControlPanelState extends State<newControlPanel> {
                       }
                     }
                   },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      labelText: 'Enter Z Position'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    labelText: 'Enter Z Position',
+                  ),
                 ),
               ),
               IconButton(
-                  onPressed: () {
-                    _incrementValue(_zposController);
-                    espDataState.updateZPos(_zposController.text as int);
-                  },
-                  icon: Icon(Icons.arrow_right))
+                onPressed: () {
+                  _incrementValue(
+                      _zposController, 6000); // Replace 100 with your max value
+                  espDataState
+                      .updateZPos(int.tryParse(_zposController.text) ?? 0);
+                },
+                icon: Icon(Icons.arrow_right),
+              ),
             ],
           ),
           const SizedBox(
@@ -228,29 +269,35 @@ class _newControlPanelState extends State<newControlPanel> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      onPrimary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  onPressed: () {
-                    espDataState.submitValues();
-                  },
-                  child: const Text('Submit Value')),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  espDataState.submitValues();
+                },
+                child: const Text('Submit Value'),
+              ),
               ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      onPrimary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  onPressed: () {
-                    _brightnessController.clear();
-                    _xposController.clear();
-                    _yposController.clear();
-                    _zposController.clear();
-                    espDataState.resetValue();
-                  },
-                  child: const Text('Reset Value')),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  _brightnessController.clear();
+                  _xposController.clear();
+                  _yposController.clear();
+                  _zposController.clear();
+                  espDataState.resetValue();
+                },
+                child: const Text('Reset Value'),
+              ),
             ],
           ),
         ],
